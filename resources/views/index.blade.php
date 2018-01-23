6f29aedd3eddca5a445086f451f71e76499ca35a
adi darmawan <badutdarmawan1711@gmail.com>
2018-01-23 07:16:22 +0700

    reskin v.3

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -563,30 +563,51 @@
 			<div class="row">
 				<div class="col-lg-3">
 					<div class="newsletter_text d-flex flex-column justify-content-center align-items-lg-start align-items-md-center text-center">
-						<div class="logo_container">
-							<img src="images/logo.png" class="logo">
-						</div>
+							<img src="images/logo.png" class="logo_footer">
 					</div>
 				</div>
 				<div class="col-lg-3">
-					<div class="newsletter_text d-flex flex-column justify-content-center align-items-lg-start align-items-md-center text-center">
+					<div class="newsletter_text">
 						<h4>About Garda Teknika</h4>
 						<p>Garda Teknika provides cutting edge technologies for universities, industries as well as research institutions. Our products are widely used in material research, metallurgy, energetic materials tests, animal research, chemical, biology and educations. </p>
 					</div>
 				</div>
 				<div class="col-lg-3">
-					<div class="newsletter_text d-flex flex-column justify-content-center align-items-lg-start align-items-md-center text-center">
-						<h4>Newsletter</h4>
-						<p>Subscribe to our newsletter and get 20% off your first purchase</p>
+					<div class="newsletter_text ">
+						<h4>Products</h4>
+						<ul>
+							<li><i class="fa fa-bars" aria-hidden="true"></i>
+								<a href="#">Material Research</a>
+							</li>
+							<li><i class="fa fa-bars" aria-hidden="true"></i>
+								<a href="#">Energetic Materials</a>
+							</li>
+							<li><i class="fa fa-bars" aria-hidden="true"></i>
+								<a href="#">General Equipments</a>
+							</li>
+							<li><i class="fa fa-bars" aria-hidden="true"></i>
+								<a href="#">Enviromental</a>
+							</li>
+							<li><i class="fa fa-bars" aria-hidden="true"></i>
+								<a href="#">Food and Agriculture</a>
+							</li>
+							<li><i class="fa fa-bars" aria-hidden="true"></i>
+								<a href="#">Pharmaceuticals</a>
+							</li>
+						</ul>
 					</div>
 				</div>
 				<div class="col-lg-3">
-					<form action="post">
-						<div class="newsletter_form d-flex flex-md-row flex-column flex-xs-column align-items-center justify-content-lg-end justify-content-center">
-							<input id="newsletter_email" type="email" placeholder="Your email" required="required" data-error="Valid email is required.">
-							<button id="newsletter_submit" type="submit" class="newsletter_submit_btn trans_300" value="Submit">subscribe</button>
-						</div>
-					</form>
+					<div class="newsletter_text">
+						<h4>Contact Us</h4>
+						<p><i class="fa fa-map-marker" aria-hidden="true"></i>
+							JL. berkah jaya no 41
+						</p>
+						<p><i class="fa fa-phone" aria-hidden="true"></i>
+							089 81293 xxxx
+						</p>
+
+					</div>
 				</div>
 			</div>
 		</div>
@@ -597,31 +618,12 @@
 	<footer class="footer">
 		<div class="container">
 			<div class="row">
-				<div class="col-lg-6">
-					<div class="footer_nav_container d-flex flex-sm-row flex-column align-items-center justify-content-lg-start justify-content-center text-center">
-						<ul class="footer_nav">
-							<li><a href="#">Blog</a></li>
-							<li><a href="#">FAQs</a></li>
-							<li><a href="contact.html">Contact us</a></li>
-						</ul>
-					</div>
-				</div>
-				<div class="col-lg-6">
-					<div class="footer_social d-flex flex-row align-items-center justify-content-lg-end justify-content-center">
-						<ul>
-							<li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
-							<li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
-							<li><a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
-							<li><a href="#"><i class="fa fa-skype" aria-hidden="true"></i></a></li>
-							<li><a href="#"><i class="fa fa-pinterest" aria-hidden="true"></i></a></li>
-						</ul>
-					</div>
-				</div>
+				
 			</div>
 			<div class="row">
 				<div class="col-lg-12">
 					<div class="footer_nav_container">
-						<div class="cr">©2018 All Rights Reserverd. This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="#">Colorlib</a></div>
+						<div class="cr">©2018  Garda Teknika. All Rights Reserved | Designed by: Kerja.in</div>
 					</div>
 				</div>
 			</div>
