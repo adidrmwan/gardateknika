6ef154add225e147d52fb982f14b2b743904307f
adi darmawan <badutdarmawan1711@gmail.com>
2018-02-01 23:59:59 +0700

    hamburger fixed

--- resources/views/layouts/master-home.blade.php
+++ resources/views/layouts/master-home.blade.php
@@ -4,7 +4,7 @@
 	<title>Garda Teknika</title>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="description" content="Colo Shop Template">
+	<meta name="description" content="">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" type="text/css" href="css/bootstrap4/bootstrap.min.css">
 	<link href="plugins/font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
@@ -26,11 +26,12 @@
 @include('layouts.master-footer')
 <script src="js/jquery-3.2.1.min.js"></script>
 <script src="css/bootstrap4/popper.js"></script>
+<script src="js/custom.js"></script>
 <script src="css/bootstrap4/bootstrap.min.js"></script>
 <script src="plugins/Isotope/isotope.pkgd.min.js"></script>
 <script src="plugins/OwlCarousel2-2.2.1/owl.carousel.js"></script>
 <script src="plugins/easing/easing.js"></script>
-<script src="js/custom.js"></script>
+
 </body>
 
 </html>
